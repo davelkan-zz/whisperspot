6ada1a9a9e5f0df69f17ed4be0fb583d92e1f834
dontworryaboutit <searing.michael@gmail.com>
2014-12-01 21:58:48 -0500

    pull nodes from Firebase

--- app/src/main/java/com/example/davelkan/mapv2/FirebaseUtils.java
+++ app/src/main/java/com/example/davelkan/mapv2/FirebaseUtils.java
@@ -1,10 +1,17 @@
 package com.example.davelkan.mapv2;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
 import com.firebase.client.ValueEventListener;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,6 +20,7 @@ import java.util.Map;
 public class FirebaseUtils {
     private String TAG = "FirebaseUtils";
     private String url = "https://mobproto-final.firebaseio.com/";
+    private Firebase firebase = new Firebase(url);
 
     public void pushUUIDInfo(String deviceAddress, Map<String, byte[]> valueMap) {
         String timeStamp = new SimpleDateFormat("yyyy:MM:dd-HH:mm:ss").format(new Date());
@@ -22,9 +30,35 @@ public class FirebaseUtils {
     }
 
     public void getNodeList(ValueEventListener listener) {
-
         Firebase nodeList = (new Firebase(url));
         nodeList = nodeList.child("nodes");
         nodeList.addListenerForSingleValueEvent(listener);
+    }
+
+    public List<MapsActivity.Point> getBlueLatList() {
+        Query queryRef = firebase.child("nodes").orderByChild("color").equalTo("blue");
+        queryRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot snapshot, String previousChild) {
+                MapsActivity.Node item = new MapsActivity.Node(snapshot.getValue(RawNode.class), snapshot.getKey());
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot snapshot) {
+            }
+
+            @Override
+            public void onCancelled(FirebaseError error) {
+            }
+        });
+        return new ArrayList<MapsActivity.Node> (Arrays.asList(items));
+    }
+
+//    pattern for Firebase objects
+    public class RawNode {
+        String color;
+        double lat, lon;
+        public RawNode() { }
     }
 }
--- app/src/main/java/com/example/davelkan/mapv2/MapsActivity.java
+++ app/src/main/java/com/example/davelkan/mapv2/MapsActivity.java
@@ -268,16 +268,25 @@ public class MapsActivity extends FragmentActivity{
         }
     }
 
-    public class Node{
+    public static class Node {
         int tag;
+        String device;
         String colour;
         Point center;
-        public Node(){
-            this.center = center;
-            this.colour = colour;
+        public Node(int tag, String device, String colour, Point center) {
             this.tag = tag;
+            this.device = device;
+            this.colour = colour;
+            this.center = center;
         }
+        public Node(FirebaseUtils.RawNode rawNode, String device) {
+            this.device = device;
+            this.colour = rawNode.color;
+            this.center = new Point(rawNode.lat, rawNode.lon);
+        }
+        public Node() { }
     }
+
     public void initButtons(){
         lvmsg = (Button) findViewById(R.id.lvmsg);
         tkmsg = (Button) findViewById(R.id.tkmsg);
